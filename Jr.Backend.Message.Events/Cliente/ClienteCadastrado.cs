91c8003a65f1a585b89543ad235181ef569b8445
João Roberto de Oliveira Rêgo <joaocte@gmail.com>
2021-10-26 15:53:39 -0300

    Add project files.

---
+++ Jr.Backend.Message.Events/Cliente/ClienteCadastrado.cs
@@ -0,0 +1,28 @@
+﻿using Jr.Backend.Libs.Messaging.Abstractions.Interfaces;
+using System.Collections.Generic;
+
+namespace Jr.Backend.Message.Events.Cliente
+{
+    public class ClienteCadastrado : IEvent
+    {
+        public string Nome { get; }
+
+        public string Sobrenome { get; }
+
+        public IList<Endereco> Enderecos { get; }
+
+        public string Cpf { get; }
+        public string Rg { get; }
+        public string TituloEleitoral { get; }
+
+        public ClienteCadastrado(string nome, string sobrenome, IList<Endereco> enderecos, string cpf, string rg, string tituloEleitoral)
+        {
+            Nome = nome;
+            Sobrenome = sobrenome;
+            Enderecos = enderecos;
+            Cpf = cpf;
+            Rg = rg;
+            TituloEleitoral = tituloEleitoral;
+        }
+    }
+}
---
+++ Jr.Backend.Message.Events/Cliente/Endereco.cs
@@ -0,0 +1,15 @@
+﻿namespace Jr.Backend.Message.Events.Cliente
+{
+    public class Endereco
+    {
+        public string Logradouro { get; }
+        public string Bairro { get; }
+        public string Numero { get; }
+        public string Estado { get; }
+        public string Cidade { get; }
+        public string Pais { get; }
+        public string Cep { get; }
+
+        public string Complemento { get; }
+    }
+}
